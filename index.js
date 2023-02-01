3af3f58a8bb4edaeba8ba72a37b247042100a0b7
sksabbirhossain <sksabbirhossain4@gmail.com>
2023-02-01 22:54:01 +0600

    change database url

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
 const blogRouter = require("./routes/blogRouter");
 
 const app = express();
 app.use(express.json());
+dotenv.config();
 app.use(cors());
 const port = process.env.PORT || 5000;
 
 //database connection
 mongoose.set("strictQuery", true);
 mongoose
-  .connect("mongodb://localhost:27017/blog")
+  .connect(
+    `mongodb+srv://daily-blog-admin:${process.env.PASSWORD}@cluster0.k4dac3z.mongodb.net/?retryWrites=true&w=majority`
+  )
   .then(() => {
     console.log("database connection successfully");
   })
